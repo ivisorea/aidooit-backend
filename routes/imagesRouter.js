fb15645d644f925aed6d708d84a2513d4914f910
Ivis Orea <ivisorea@gmail.com>
2022-01-14 11:16:02 +0100

    Delete s3 client

--- routes/imagesRouter.js
+++ routes/imagesRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getImageS3, uploadsResponse } from "../controllers/images.js";
+import { uploadsResponse } from "../controllers/images.js";
 import imageUploader from "../middleware/imageUploader.js";
 import verifyToken from "../middleware/verifyToken.js";
 import { upload } from "../middleware/imageUploaderS3.js";
@@ -9,6 +9,5 @@ const imagesRouter = Router();
 
 imagesRouter.post("/", verifyToken, imageUploader.single('file'), uploadsResponse);
 imagesRouter.post("/s3",verifyToken, upload.array('file'), uploadsResponseS3);
-imagesRouter.get("/:Key", getImageS3);
 
 export default imagesRouter;
